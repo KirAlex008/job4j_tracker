67f0cc51b6ddfc706ae92dc9fef30f0737755705
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-05-04 13:55:14 +0300

    file Bug

---
+++ src/main/java/ru/job4j/tracker/Bug.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+public class Bug extends Item {
+    public Bug(String name) {
+        super(name);
+    }
+    public static void main(String[] args) {
+        Bug bug = new Bug("1");
+    }
+}
