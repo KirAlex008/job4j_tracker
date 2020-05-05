5af6ff7eb57005e7f60848a3b3a3bd1c760512e0
KirAlex008 <59695224+KirAlex008@users.noreply.github.com>
2020-05-05 16:34:45 +0300

    9. Написать тесты на StartUI. [#269827]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,53 +3,15 @@ package ru.job4j.tracker;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.core.IsNull.nullValue;
-import static org.junit.Assert.*;
-import org.hamcrest.core.IsNull;
 
 public class StartUITest {
-    /**
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC", "Fix PC2", "Fix PC3" };
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        StartUI.createItem(input, tracker);
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[2];
-        Item expected = new Item("Fix PC3");
-        assertThat(created.getName(), is(expected.getName()));
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
-
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                "replaced item",
-                item.getId() // id сохраненной заявки в объект tracker.
-        };
-        Input input = new StubInput(answers);
-        StartUI.replaceItem(input, tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-
-                item.getId() // id сохраненной заявки в объект tracker.
-        };
-        Input input = new StubInput(answers);
-        StartUI.deteleItem(input, tracker);
-        Item result = tracker.findById(item.getId());
-        assertThat(result, (is(IsNull.nullValue())));
-    }
-    **/
 }
